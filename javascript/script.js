39cf794164dde131299378a97e971a62b6a256fe
Akadeha420 <117671840+Akadeha420@users.noreply.github.com>
2023-11-14 21:35:11 -0600

    added map

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+function initMap() {
+  const myLatLng = {lat: 18.3802694, lng: 94.3340317};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
